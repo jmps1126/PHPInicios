ef8c1f15fb860655fdd6fee98fd1dec6a79afce7
jmps1126 <jmps1126@gmail.com>
2017-03-14 16:51:42 -0500

    Modificación index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Práctica PHP</title>
+	<title>Práctica PHP GIT</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
